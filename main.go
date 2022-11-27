f6246eb218c3b60a03d8a985753666f63f337dd1
Gileno <gileno.cr.duarte2gmail.com>
2022-11-27 15:23:38 -0300

    dividindo responsabilidades em funções

--- main.go
+++ main.go
@@ -2,37 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	nome := "Gileno"
-	versao := 1.1
-	fmt.Println("Ola ", nome)
-	fmt.Println("Este programa está na versão: ", versao)
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("0 - sair do Programa")
-	var opcao int
+	exibeIntroducao()
+	exbiMenu()
 
-	//fmt.Scanf("%d", &opcao)
-
-	//leitura de input'
-	fmt.Scan(&opcao)
-	fmt.Println("A operação escolhida foi", opcao)
-
-	/*if opcao == 1 {
-		fmt.Println("Monitorando...")
-
-	} else if opcao == 2 {
-		fmt.Println("Exibindo Logs")
-
-	} else if opcao == 0 {
-		fmt.Println("Saindo do programa")
-
-	} else {
-		fmt.Println("Comando não pertence as opções válidas")
-	}*/
-
+	opcao := capturaOpcao()
 	switch opcao {
 	case 1:
 		fmt.Println("Monitorando...")
@@ -41,7 +18,49 @@ func main() {
 
 	case 0:
 		fmt.Println("Saindo do programa")
+		os.Exit(0)
 	default:
 		fmt.Println("Comando não pertence as opções válidas")
+		os.Exit(-1)
 	}
 }
+
+func exibeIntroducao() {
+	nome := "Gileno"
+	versao := 1.1
+	fmt.Println("Ola ", nome)
+	fmt.Println("Este programa está na versão: ", versao)
+
+}
+
+func capturaOpcao() int {
+	var opcao int
+	fmt.Scan(&opcao)
+	fmt.Println("A operação escolhida foi", opcao)
+
+	return opcao
+
+}
+
+func exbiMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - sair do Programa")
+}
+
+//fmt.Scanf("%d", &opcao)
+
+//leitura de input'
+
+/*if opcao == 1 {
+	fmt.Println("Monitorando...")
+
+} else if opcao == 2 {
+	fmt.Println("Exibindo Logs")
+
+} else if opcao == 0 {
+	fmt.Println("Saindo do programa")
+
+} else {
+	fmt.Println("Comando não pertence as opções válidas")
+}*/
